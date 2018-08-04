a3d5804b3b944eded6744ae0ba3aba050545fa5f
Carlos Mendes <carls.mendes@gmail.com>
2018-08-04 14:13:51 +0100

    index and show of restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'restaurants#index'
+
+  resources :restaurants, only: [ :index, :show ]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
